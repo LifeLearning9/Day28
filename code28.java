f33de31db61d8b22371ea8e1799f29a985a77716
LifeLearning <pprogram2314@gmail.com>
2025-09-02 21:53:01 +0530

    Create code28.java

---
+++ code28.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Main {
+    public static List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+        if (s.length() < p.length()) return result;
+
+        int[] pCount = new int[26];
+        int[] sCount = new int[26];
+
+        for (char ch : p.toCharArray()) {
+            pCount[ch - 'a']++;
+        }
+
+        int window = p.length();
+
+        for (int i = 0; i < s.length(); i++) {
+            sCount[s.charAt(i) - 'a']++;
+
+           
+            if (i >= window) {
+                sCount[s.charAt(i - window) - 'a']--;
+            }
+
+            
+            if (Arrays.equals(pCount, sCount)) {
+                result.add(i - window + 1);
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = "cbaebabacd";
+        String p = "abc";
+        System.out.println(findAnagrams(s, p));  // [0, 6]
+
+        s = "abab";
+        p = "ab";
+        System.out.println(findAnagrams(s, p));  // [0, 1, 2]
+    }
+}
